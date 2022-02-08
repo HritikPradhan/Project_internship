de3f5a7b1957b3da81789234d79d71601bcd7d94
Hritik Pradhan <93572272+HritikPradhan@users.noreply.github.com>
2022-02-08 20:44:14 +0530

    Update internController.js

--- src/controllers/internController.js
+++ src/controllers/internController.js
@@ -5,7 +5,6 @@ const validator = require("../validator/validator");
 //POST/functionup/interns
 const createIntern = async function (req, res) {
   try {
-    res.setHeader('Access-Control-Allow-Origin','*');
     let data = req.body;
     if (!validator.isValidRequestBody(data)) {
       //validating whether data is given in request body or not
